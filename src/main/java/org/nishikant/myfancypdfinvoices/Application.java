c6b9cf13263aa0aea510da19f9566af90aba512d
JaxonTeller <nishuzephyrus@outlook.com>
2023-04-24 13:12:57 +0530

    Our own constructor based dependency injection

--- src/main/java/org/nishikant/myfancypdfinvoices/Application.java
+++ src/main/java/org/nishikant/myfancypdfinvoices/Application.java
@@ -10,7 +10,12 @@ public class Application {
     * only one instance of invoiceSe and objectMa will be available per
     * classloader
     * and final means they can be assigned only once*/
-    public static final InvoiceService invoiceService = new InvoiceService();
-    public static final ObjectMapper objectMapper = new ObjectMapper();
+
+    /*UserService being created first as InvoiceService needs it -> but what if UserService also depends on some other
+    * dependency then we need to create it first and pass to UserService and then pass UserService to InvoiceService
+    * making this application class to bulky and wildly
+    * ---> that's where Spring comes in to replace this Application class*/
     public static final UserService userService = new UserService();
+    public static final InvoiceService invoiceService = new InvoiceService(userService);
+    public static final ObjectMapper objectMapper = new ObjectMapper();
 }
--- src/main/java/org/nishikant/service/InvoiceService.java
+++ src/main/java/org/nishikant/service/InvoiceService.java
@@ -11,12 +11,25 @@ public class InvoiceService {
 
     //Thread-safe list
     private List<Invoice> invoices = new CopyOnWriteArrayList<>();
+
+    private final UserService userService;
+
+    public InvoiceService(UserService userService) {
+        this.userService = userService;
+    }
+
     public Invoice generateInvoicePdf(String userId, Integer amount) throws Exception {
         /*
         call's a 3rd party library which generates PDF from userId and amount
         and upload it to CDN and return the URL of it.
         */
-        User user = Application.userService.findById(userId);
+        /*
+        the dependency has been delegated to Application class invoice service class still has
+        to know where it can find the dependency, to avoid this we can add constructor based injection.
+        i.e, we force the rule that if invoice service creation requires user service, so whoever creates invoice service,
+        needs to pass user-service as well -> i.e, they need to inject the dependency.
+        */
+        User user = userService.findById(userId);
         if(user==null){
             throw new Exception("User not found");
         }
